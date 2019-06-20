5bfe59918f6728c81a2f4933b6ca73cfe0db8570
Harmeet Matharoo <39014834+hmjatt@users.noreply.github.com>
2019-06-20 18:16:58 -0500

    new layout
    changed navbar, header, body layout, footer, add numbers fact app

--- widgets/bookmarker/js/main.js
+++ widgets/bookmarker/js/main.js
@@ -84,7 +84,7 @@ function fetchBookmarks(){
 
     bookmarksResults.innerHTML += '<div class="well">'+
                                   '<h3>'+name+
-                                  ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+                                  ' <a class="btn btn-info ml-5" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
                                   ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
                                   '</h3>'+
                                   '</div>';
